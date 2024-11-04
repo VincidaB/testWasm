7952d341f158cc9dff91be4dff3c5fa3b6230cf1
Vincent Belpois <vincent.belpois@gmail.com>
2024-11-03 19:02:44 -0600

    updated website a bit

--- go2_canvas.js
+++ go2_canvas.js
@@ -2269,7 +2269,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('go2_canvas_bg.wasm', import.meta.url);
+        module_or_path = new URL('./go2_canvas_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
